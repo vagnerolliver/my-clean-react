52c9adbde376b9c663f03ee19f1675d05677ae45
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-17 20:56:02 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export { ValidationComposite } from './validateComposite/validationComposite'
+export { MinLengthValidation } from './minLength/minLenght'
+export { RequiredFieldValidation } from './requiredField/requiredField'
+export { EmailValidation } from './email/email'
