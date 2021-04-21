4b8f1d722c50be82b154c00f4a38e5ab0109521a
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-21 15:40:50 -0300

    refactor: change api url factory to receive path by param

--- src/main/factories/usecases/authentication/remoteAuthenticationFactory.ts
+++ src/main/factories/usecases/authentication/remoteAuthenticationFactory.ts
@@ -4,5 +4,5 @@ import { makeApiUrl } from '@/main/factories/http/apiUrlFactory'
 import { Authentication } from '@/domain/usecases'
 
 export const makeRemoteAuthentication = (): Authentication => {
-  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient())
+  return new RemoteAuthentication(makeApiUrl('/login'), makeAxiosHttpClient())
 }
