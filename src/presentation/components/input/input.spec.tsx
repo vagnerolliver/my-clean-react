96998397866ae7942f8d30beda30c32240ca3554
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-21 15:30:53 -0300

    test: ensure Input Component loads with readOnly

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Input from './input'
 import { FormContext } from '@/presentation/contexts'
 
+const selectors = {
+  input: 'input__field'
+}
+
+const makeSut = (): RenderResult => {
+  return render(
+    <FormContext.Provider value={{ state: {} }}>
+      <Input name="field" />
+    </FormContext.Provider>
+  )
+}
+
 describe('Input Component', () => {
   test('Should begin with readOnly', () => {
-    const { getByTestId } = render(
-      <FormContext.Provider value={{ state: {} }}>
-        <Input name="field" />
-      </FormContext.Provider>
-    )
-
-    const input = getByTestId('input__field') as HTMLInputElement
+    const { getByTestId } = makeSut()
+    const input = getByTestId(selectors.input) as HTMLInputElement
     expect(input.readOnly).toBe(true)
   })
 })
