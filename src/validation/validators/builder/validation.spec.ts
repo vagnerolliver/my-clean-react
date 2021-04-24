ca8f469926b6064ee4a0b8e94957f54f20a01155
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-24 13:42:26 -0300

    fix: remove deprecated faker.random to fake.datatype

--- src/validation/validators/builder/validation.spec.ts
+++ src/validation/validators/builder/validation.spec.ts
@@ -17,14 +17,14 @@ describe('ValidationBuilder', () => {
 
   test('Should return MinLengthValidation', () => {
     const field = faker.database.column()
-    const length = faker.random.number()
+    const length = faker.datatype.number()
     const validations = sut.field(field).minLength(length).build()
     expect(validations).toEqual([new MinLengthValidation(field, length)])
   })
 
   test('Should return a list of validations', () => {
     const field = faker.database.column()
-    const length = faker.random.number()
+    const length = faker.datatype.number()
     const validations = sut.field(field).required().minLength(length).email().build()
     expect(validations).toEqual([
       new RequiredFieldValidation(field),
