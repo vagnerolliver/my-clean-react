db284463a71213c322fd8811d153293142cf4d4d
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-09 11:09:23 -0300

    feat: ensure CompareFieldValidation return InvalidFieldError if compare is invalid

--- src/validation/validators/compareFields/compareFieldsValidation.ts
+++ src/validation/validators/compareFields/compareFieldsValidation.ts
@@ -2,7 +2,7 @@ import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols/fieldValidation'
 
 export class CompareFieldsValidation implements FieldValidation {
-  constructor (readonly field: string) {}
+  constructor (readonly field: string, private readonly value: string) {}
 
   validate (value: string): Error {
     return new InvalidFieldError()
