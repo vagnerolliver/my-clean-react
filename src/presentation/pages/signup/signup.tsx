66c5e4300944d48d1df5bfccefd15153f4bcfcab
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-12 21:16:12 -0300

    feat: ensure Signup initial start with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
 import Styles from './signup.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import { FormContext } from '@/presentation/contexts'
 
 const Signup: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: 'Campo Obrigatório',
+    emailError: 'Campo Obrigatório',
+    passwordError: 'Campo Obrigatório',
+    passwordConfirmationError: 'Campo Obrigatório',
+    mainError: ''
+  })
+
   return (
-    <FormContext.Provider value={{ state: {} }}>
+    <FormContext.Provider value={{ state }}>
       <div className={Styles.signup}>
         <LoginHeader />
         <form data-testid="signup__form" className={Styles.form}>
@@ -15,8 +23,8 @@ const Signup: React.FC = () => {
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
           <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
-          <button data-testid="signup__submit" className={Styles.submit} type="submit">Criar Conta</button>
-          <Link data-testid="signup__back-login" to="/login" className={Styles.link}>Voltar Login</Link>
+          <button data-testid="signup__submit" className={Styles.submit} type="submit" disabled>Criar Conta</button>
+          <span data-testid="signup__back-login" to="/login" className={Styles.link}>Voltar Login</span>
           <FormStatus />
         </form>
         <Footer />
