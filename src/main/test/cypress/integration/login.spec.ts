4d1cf16cc23fe1cb8ad2a7af222ba5e994cff685
Vagner Silveira de Oliveira <vagner.olliver@yahoo.com.br>
2021-06-30 09:15:04 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,26 @@
 describe('Login', () => {
-  it('Should load with corret initial state', () => {
-    cy.visit('Login')
+  const selectors = {
+    form: 'login__form',
+    submitButton: 'submitButton',
+    signupLinkElement: 'login__signup',
+    errorWrap: 'form-status__error-wrap',
+    mainError: 'form-status__main-error',
+    emailInput: 'input__email',
+    passwordInput: 'input__password',
+    emailInputStatus: 'input__email-status',
+    passwordInputStatus: 'input__password-status',
+    spinnerElement: 'spinner'
+  }
+  beforeEach(() => cy.visit('Login'))
+
+  it('Should load with correct initial state', () => {
+    cy.getByTestId(selectors.emailInputStatus)
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId(selectors.passwordInputStatus)
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId(selectors.submitButton).should('have.attr', 'disabled')
+    cy.getByTestId(selectors.errorWrap).should('not.have.descendants')
   })
 })
